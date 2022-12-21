3eb02893822e67e557fa48844149b1f8e3d5d880
jimmyhealer <yahing6066@gmail.com>
2022-12-21 22:05:24 +0800

    refactor: change router

--- main.go
+++ main.go
@@ -16,17 +16,7 @@ func main() {
 	database.SetupFirebase()
 	auth.Auth(database.Key)
 
-	auth_router := server.Group("/")
-	router.AddAuthRouter(auth_router)
-
-	project_router := server.Group("user/:user_email/project")
-	router.AddProjectRouter(project_router)
-
-	repo_router := project_router.Group("/:project_name/repo")
-	router.AddRepoRouter(repo_router)
-
-	kanban_router := project_router.Group("/:project_name/kanban")
-	router.AddKanbanRouter(kanban_router)
+	router.Init(server)
 
 	server.Run(":8000")
 }
--- router/router.go
+++ router/router.go
@@ -6,47 +6,56 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func AddAuthRouter(auth_router *gin.RouterGroup) {
-	auth_router.POST("/signup/", service.UserRegister)
-	auth_router.POST("/forgot/", service.UserForgotPassword)
-	auth_router.POST("/signin/", middleware.AuthMiddleware.LoginHandler)
-	auth_router.POST("/signout/", middleware.AuthMiddleware.LogoutHandler)
-	auth_router.POST("/refresh/", middleware.AuthMiddleware.RefreshHandler)
-	auth_router.Use(middleware.AuthMiddleware.MiddlewareFunc())
+func Init(server *gin.Engine) {
+	auth_router := server.Group("/")
 	{
-		auth_router.GET("/hello", middleware.HelloHandler)
+		auth_router.POST("/signup/", service.UserRegister)
+		auth_router.POST("/forgot/", service.UserForgotPassword)
+		auth_router.POST("/signin/", middleware.AuthMiddleware.LoginHandler)
+		auth_router.POST("/signout/", middleware.AuthMiddleware.LogoutHandler)
+		auth_router.POST("/refresh/", middleware.AuthMiddleware.RefreshHandler)
 	}
-}
 
-func AddProjectRouter(project_router *gin.RouterGroup) {
-	project_router.GET("/", service.ListAllProjects)
-	project_router.GET("/:project_name/", service.GetProject)
-	project_router.Use(middleware.MemberCheck())
+	project_router := server.Group("/user/:user_email/project")
 	{
-		project_router.PUT("/:project_name/", service.UpdateProject)
-		project_router.POST("/:project_name/member/", service.AddMember)
-	}
-	project_router.Use(middleware.AuthCheck())
-	{
-		project_router.POST("/", service.CreateProject)
-		project_router.DELETE("/:project_name/", service.DeleteProject)
-	}
-}
+		project_router.GET("/", service.ListAllProjects)
+		project_router.GET("/:project_name/", service.GetProject)
 
-func AddRepoRouter(repo_router *gin.RouterGroup) {
-	repo_router.GET("/", service.ListAllRepos)
-	repo_router.Use(middleware.MemberCheck())
-	{
-		repo_router.POST("/", service.AddRepo)
-	}
-}
+		project_member_router := server.Group("/user/:user_email/project/").Use(middleware.MemberCheck())
+		{
+			project_member_router.PUT("/:project_name/", service.UpdateProject)
+			project_member_router.POST("/:project_name/member/", service.AddMember)
+		}
 
-func AddKanbanRouter(kanban_router *gin.RouterGroup) {
-	kanban_router.GET("/", service.ListKanban)
-	kanban_router.Use(middleware.MemberCheck())
+		project_owner_router := server.Group("/user/:user_email/project/").Use(middleware.OwnerCheck())
+		{
+			project_owner_router.POST("/", service.CreateProject)
+			project_owner_router.DELETE("/:project_name/", service.DeleteProject)
+		}
+	}
+
+	repo_router := project_router.Group("/:project_name/repo")
 	{
-		kanban_router.PUT("/", service.AddWorkFlow)
-		kanban_router.POST("/", service.AddTask)
-		kanban_router.DELETE("/", service.DeleteTask)
+		repo_router.GET("/", service.ListAllRepos)
+
+		repo_member_router := repo_router.Use(middleware.MemberCheck())
+		{
+			// repo_member_router.GET("/:repo_name/", service.GetRepo)
+			repo_member_router.POST(" /", service.CreateRepo)
+		}
+	}
+
+	kanban_router := project_router.Group("/:project_name/kanban")
+	{
+		kanban_router.GET("/", service.ListAllKanbans)
+
+		kanban_member_router := kanban_router.Use(middleware.MemberCheck())
+		{
+			// kanban_member_router.GET("/", service.GetKanban)
+			// kanban_member_router.POST("/", service.CreateKanban)
+			kanban_member_router.PUT("/", service.AddWorkFlow)
+			kanban_member_router.POST("/", service.AddTask)
+			kanban_member_router.DELETE("/:workflow_name/:task_id/", service.DeleteTask)
+		}
 	}
 }
